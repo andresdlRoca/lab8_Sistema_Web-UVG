957428accab2cd4f51f1bfa32a574e0ed8a259c4
andresdlRoca <andresdelaroca11@gmail.com>
2022-03-23 17:25:25 -0600

    Very basic UI implementations

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,8 +4,9 @@ import './Game.css';
 class Game extends Component {
     render() {
         return (
-        <div>
-            <h1>Bienvenido al juego de memoria!</h1>
+        <div className='mainUI'>
+            <h1 id='titulo'>Bienvenido al juego de memoria!</h1>
+            <button id="newgameBtn">Nueva Partida</button>
         </div>
         );
         
