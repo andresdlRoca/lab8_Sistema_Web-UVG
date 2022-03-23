fdd0afafea71e11aff9abcf3dd167f20789b8ab3
andresdlRoca <andresdelaroca11@gmail.com>
2022-03-23 16:59:50 -0600

    Initial commit

---
+++ src/components/Game.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Game.css';
+
+class Game extends Component {
+    render() {
+        return (
+        <div>
+            <h1>Bienvenido al juego de memoria!</h1>
+        </div>
+        );
+        
+    }
+
+}
+
+export default Game;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Game from './components/Game';
+import "./index.css";
+import background from './assets/background.png'
+
+ReactDOM.render(
+    <Game />
+    
+    
+    
+    
+
+    
+    , document.getElementById('game'));
